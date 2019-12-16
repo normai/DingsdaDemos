4057f842e36f19bc89b4ed9d6eb464e0fa4b4584
normai <n_maier@compuserve.com>
2019-12-16 15:14:07 +0100

    Copy folder

---
+++ InterDings/Amphibious.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * [file 20191212°0311]
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interdingsda
+{
+   /// <summary>
+   /// This class constitues an amphibious vehicle, it can wheel and swimm
+   /// </summary>
+   /// <remarks>[class 20191212°0313]</remarks>
+   class Amphibious : Automobile, ISwimmable
+   {
+      /// <summary>
+      /// This constructor's sole purpose is to put the new amphibious vehicle automatically on the Swimmables list.
+      /// </summary>
+      /// <remarks>[class 20191212°0315]</remarks>
+      public Amphibious()
+      {
+         Dingsda.swimmables.Add(this);
+      }
+
+      /// <summary>
+      /// This method needs to exist due to the iSwimmable interface.
+      /// </summary>
+      /// <remarks>[method 20191212°0321]</remarks>
+      public void Swimm()
+      {
+         string s = " - " + this.Me + " turns the amphibian propeller.";
+         Console.WriteLine(s);
+      }
+   }
+}
