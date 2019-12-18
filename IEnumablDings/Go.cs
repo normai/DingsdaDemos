d75a41d25a20a5aeb56ae94ab9a5e0e5954d66cf
normai <n_maier@compuserve.com>
2019-12-18 14:21:12 +0100

    Demo implementing IEnumerable<> runs proof-of-concept

--- IEnumablDings/Go.cs
+++ IEnumablDings/Go.cs
@@ -1,6 +1,6 @@
 ﻿/*
- * This project shall demonstrate subtleties of the IEnumerable interface
- * [file 20191218°0221][project 20191218°0311]
+ * This class shall work off some IEnumerable demos
+ * [file 20191218°0311]
  */
 
 using System;
@@ -16,18 +16,49 @@ namespace IEnumablDings
    /// <summary>
    /// This static class holds the functionality of this project.
    /// </summary>
-   /// <remarks>[class 20191218°0321]</remarks>
+   /// <remarks>[class 20191218°0313]</remarks>
    public static class IEnumablDemo
    {
       /// <summary>
       /// This method performs the wanted demos ..
       /// </summary>
-      /// <remarks>[method 20191218°0331]</remarks>
+      /// <remarks>[method 20191218°0321]</remarks>
       public static void Go()
       {
          Console.WriteLine("Go ..");
 
-         Automobile a = new Automobile();
+         // akquire an enumerator [seq ]
+         Thingies t = new Thingies();
+         IEnumerator<string> en1 = t.GetEnumerator();
+         /*
+         string s1 = en1.Current;
+         en1.MoveNext();
+         string s2 = en1.Current;
+         en1.MoveNext();
+         string s3 = en1.Current;
+         en1.Reset();
+         en1.Dispose();
+         */
+
+         /*
+         todo : What happens, if the Enumerator were empty, and would
+           not yield any value even on the first use of en1.Current?
+           How can such situation be handled? [todo 20191218°1231]
+         */
+
+         // use an enumerator [seq ]
+         Console.WriteLine("Using IEnumerator<string> en1 = t.GetEnumerator();");
+         do
+         {
+            Console.WriteLine(" - " + en1.Current);
+         }
+         while (en1.MoveNext());
+
+         // try it with LINQ [seq ]
+         /*
+         IEnumerator<string> en2 = t.GetEnumerator();
+         var x = from s in en2 select s;
+         */
 
       }
    }
---
+++ IEnumablDings/Thingies.cs
@@ -0,0 +1,189 @@
+﻿/*
+ * file : 20191218°0411
+ * summary : This class shall demonstrate the IEnumerable interface
+ * ref : See e.g. MS Docs article 'IEnumerable<T> Interface' [ref 20191218°1212]
+ *    https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ienumerable-1
+ *    This article provides a big example code.
+ * ref : See e.g. MS Docs article 'IEnumerator Interface' [ref 20191218°1212]
+ *     https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerator?view=netframework-4.6.1
+ * note : This code was started after fragments from ref 20191218°1212, then
+ *       supplemented by the VS hint button to automatically rq   esolve problems.
+ *       Those parts are that with the german comments, since it was a german VS.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumablDings
+{
+   /// <summary>
+   /// This class catalogues a list of classes and it implements the IEnumerable interface.
+   /// </summary>
+   /// <remarks>[file 20191218°0413]</remarks>
+   public class Thingies : IEnumerable<string>
+   {
+      /// <summary>
+      /// This default constructor provides one Thingies object
+      /// </summary>
+      /// <remarks>[ctor 20191218°0421]</remarks>
+      public Thingies()
+      {
+         Console.WriteLine("Create Thingies " + this.GetHashCode());
+      }
+
+      /// <summary>
+      /// This method satisfies one interface requirement
+      /// </summary>
+      /// <remarks>[method 20191218°0431]</remarks>
+      public IEnumerator<string> GetEnumerator()
+      {
+         IEnumerator<string> ieo = new ThingiesEnumerator();
+         return ieo;
+      }
+
+      /// <summary>
+      /// (This method was introduced by VS-Hint)
+      /// </summary>
+      /// <returns>[method 20191218°0433]</returns>
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+         throw new NotImplementedException();
+      }
+   }
+
+   /// <summary>
+   /// This class is wanted by the other class which implements IEnumerable<>.
+   /// Note: Instead implementing IEnumerator<string>, we could also use keyword 'yield'
+   /// </summary>
+   /// <remarks>[class 20191218°0511]</remarks>
+   public class ThingiesEnumerator : IEnumerator<string>
+   {
+      /// <summary>
+      /// This field ...
+      /// </summary>
+      /// <returns>[field 20191218°0522]</returns>
+      private int iCurrIndex = 0;
+
+      /// <summary>
+      /// This field ...
+      /// </summary>
+      /// <returns>[field 20191218°0524]</returns>
+      private string[] aOutStrings = {"Hello", "Hallo", "Aloha", "Gruezi", "Hujambo" };
+
+      // Implement the IEnumerator(T).Current publicly, but implement 
+      // IEnumerator.Current, which is also required, privately.
+      /// <summary>
+      /// This method (was introduced by VS-Hint) ...
+      /// </summary>
+      /// <returns>[field 20191218°0526]</returns>
+      public string Current
+      {
+         get
+         {
+            /*
+            if (_sr == null || _current == null)
+            {
+               throw new InvalidOperationException();
+            }
+            return _current;
+            */
+            return aOutStrings[iCurrIndex];
+         }
+      }
+
+      /// <summary>
+      /// This property (was introduced by VS-Hint) ...
+      /// </summary>
+      /// <returns>[property 20191218°0528]</returns>
+      ////object IEnumerator.Current => throw new NotImplementedException();
+      object IEnumerator.Current => aOutStrings[iCurrIndex];
+
+      /// <summary>
+      /// This method ...
+      /// </summary>
+      /// <returns>[method 20191218°0532]</returns>
+      public bool MoveNext()
+      {
+         ////throw new NotImplementedException();
+         bool bRet = false;
+         iCurrIndex++;
+         if (iCurrIndex < aOutStrings.Length )
+         {
+            bRet = true;
+         }
+         return bRet;
+      }
+
+      /// <summary>
+      /// This method (was introduced by VS-Hint) ...
+      /// </summary>
+      /// <returns>[field 20191218°0534]</returns>
+      public void Reset()
+      {
+         ////throw new NotImplementedException();
+         iCurrIndex = 0;
+      }
+
+      #region IDisposable Support
+
+      /// <summary>
+      /// This field (was introduced by VS-Hint) ...
+      /// </summary>
+      /// <returns>[field 20191218°0536]</returns>
+      private bool disposedValue = false; // Dient zur Erkennung redundanter Aufrufe.
+
+      /// <summary>
+      /// This method (was introduced by VS-Hint) ...
+      /// </summary>
+      /// <returns>[method 20191218°0538]</returns>
+      protected virtual void Dispose(bool disposing)
+      {
+         if (!disposedValue)
+         {
+            if (disposing)
+            {
+               // TODO: verwalteten Zustand (verwaltete Objekte) entsorgen.
+            }
+
+            // TODO: nicht verwaltete Ressourcen (nicht verwaltete Objekte)
+            //    freigeben und Finalizer weiter unten überschreiben.
+            // TODO: große Felder auf Null setzen.
+
+            disposedValue = true;
+         }
+      }
+
+      // [todo 20191218°0542] (This todo was introduced by VS-Hint)
+      // TODO: Finalizer nur überschreiben, wenn Dispose(bool disposing) weiter
+      //        oben Code für die Freigabe nicht verwalteter Ressourcen enthält.
+      /// <summary>
+      /// This method (was introduced by VS-Hint) ...
+      /// </summary>
+      /// <returns>[method 20191218°0544]</returns>
+      // ~ThingiesEnumerator() {
+      //   // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode
+      //   //    in Dispose(bool disposing) weiter oben ein.
+      //   Dispose(false);
+      // }
+
+      // Dieser Code wird hinzugefügt, um das Dispose-Muster richtig zu implementieren.
+      /// <summary>
+      /// This method (was introduced by VS-Hint) ...
+      /// </summary>
+      /// <returns>[method 20191218°0546]</returns>
+      public void Dispose()
+      {
+         // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode
+         //   in Dispose(bool disposing) weiter oben ein.
+         Dispose(true);
+         // TODO: Auskommentierung der folgenden Zeile aufheben, wenn
+         //        der Finalizer weiter oben überschrieben wird.
+         // GC.SuppressFinalize(this);
+      }
+      #endregion
+   }
+}
